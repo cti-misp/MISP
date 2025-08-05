9d66b1c5c55c509cb68d6cb2320b223191f235d8
cti-misp <cti_misp@ncsa.or.th>
2025-08-05 14:53:07 +0700

    Delete Labs/MOPH/test_wazuh_fim.bat

--- Labs/MOPH/test_wazuh_fim.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Fake malicious behavior for Wazuh FIM testing
-echo Starting dummy operations...
-mkdir "%TEMP%\WazuhTestFolder"
-echo "Wazuh FIM test" > "%TEMP%\WazuhTestFolder\testfile.txt"
-del "%TEMP%\WazuhTestFolder\testfile.txt"
-rmdir "%TEMP%\WazuhTestFolder"
-echo Completed test operations.
-pause
